38924a4325322d2661b9efd53cfb7b1b07b42983
Gabriel S <gcsprog@gmail.com>
2025-11-29 14:59:55 -0300

    feat: adicionar novo remote

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -10,6 +10,12 @@ export const routes: Routes = [
         .then(m => m.AppRoutingModule)
   },
   {
+    path: 'dashboard-admin',
+    loadChildren: () =>
+      loadRemoteModule('dashboard-admin', './AppRoutingModule')
+        .then(m => m.Routes)
+  },
+  {
     path: 'login',
     loadComponent: () => import('./features/auth/login/login').then(m => m.Login),
   }
